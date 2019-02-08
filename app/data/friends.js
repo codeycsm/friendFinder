d168b245cc8301e938aee222fee3d27d8efec77b
Codey <codeymcdaniel@gmail.com>
2019-02-08 08:56:35 -0700

    initialized and majority of the web app completed

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+let friends = [
+  {
+    name: "friend1",
+    image:
+      "https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg",
+    answers: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "friend2",
+    image:
+      "https://assets3.thrillist.com/v1/image/2618561/size/tmg-article_tall;jpeg_quality=20.jpg",
+    answers: ["1", "2", "3", "4", "5", "4", "3", "2", "1", "2"]
+  },
+  {
+    name: "friend3",
+    image:
+      "https://www.nps.gov/glba/learn/nature/images/2Bear_2.jpg?maxwidth=650&autorotate=false",
+    answers: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  }
+];
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,20 @@
+// DONT FORGET TO EXPORT YOUR FILES!!!!!
+// Required packages
+let express = require("express"),
+  path = require("path"),
+  htmlRoutes = require("./app/routing/htmlRoutes"),
+  apiRoutes = require("./app/routing/apiRoutes");
+
+// Initialize express app and port to listen on.
+let app = express(),
+  PORT = 8000;
+
+// express body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, function() {
+  console.log(`Listening on port: ${PORT}`);
+});
